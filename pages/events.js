1bda896700f252251bb22ebadde66c3d112970bc
reggreen <redgreenonline2023@gmail.com>
2026-07-06 13:43:24 +0600

    Create events.js

---
+++ pages/events.js
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar';
+
+export default function Events() {
+  return (
+    <>
+      <Navbar />
+      <h1>Events & Offers</h1>
+      <div>
+        <h3>Eid Special Package</h3>
+        <p>50% Discount</p>
+      </div>
+    </>
+  );
+}
