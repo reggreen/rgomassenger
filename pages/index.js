6420c6c9687fe9f47af4c42d58a705c5951f0daf
reggreen <redgreenonline2023@gmail.com>
2026-07-06 14:02:19 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <h1>Community Home</h1>
+      <h1>rgomassenger Home</h1>
       <p>আজ রাত ১২টা থেকে সার্ভার মেইনটেন্যান্স হবে।</p>
     </>
   );
