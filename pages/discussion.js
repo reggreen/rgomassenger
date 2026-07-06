da315a24e635e684cfa8e3c8f56fdd1da34de20b
reggreen <redgreenonline2023@gmail.com>
2026-07-06 13:43:53 +0600

    Create discussion.js

---
+++ pages/discussion.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar';
+
+export default function Discussion() {
+  return (
+    <>
+      <Navbar />
+      <h1>General Discussion</h1>
+      <p>এখানে গ্রাহকরা তাদের অভিজ্ঞতা শেয়ার করতে পারবে।</p>
+    </>
+  );
+}
