3be1936c3f79d2375de679023cf7683b46091df5
reggreen <redgreenonline2023@gmail.com>
2026-07-06 13:41:34 +0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <h1>Community Home</h1>
+      <p>আজ রাত ১২টা থেকে সার্ভার মেইনটেন্যান্স হবে।</p>
+    </>
+  );
+}
